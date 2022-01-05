e95ee18a3509f7384bfc40a3e79d7d3ae942d911
Amit Gajbhiye <amitgajbhiye>
2022-01-05 15:50:22 +0000

    added early stopping

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-#SBATCH -p dev
+#SBATCH -p gpu_v100
 #SBATCH --mem=10g
-##SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1
 ##SBATCH --qos=gpu7d
-#SBATCH -t 0-01:00:00
+#SBATCH -t 0-13:00:00
 
 echo 'This script is running on:'
 hostname
