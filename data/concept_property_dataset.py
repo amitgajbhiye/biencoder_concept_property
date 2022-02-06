5b810ec686014d79b685d71148fb0780020aac68
Amit Gajbhiye <amitgajbhiye>
2022-02-06 16:17:12 +0000

    removed add_context import

--- data/concept_property_dataset.py
+++ data/concept_property_dataset.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
-from utils.functions import add_context
+
 
 log = logging.getLogger(__name__)
 
