b5de919aa52825361d860402218fd867bb8c3af1
Amit Gajbhiye <amitgajbhiye>
2022-02-28 17:34:09 +0000

    bert large without finetuning

--- finetune_run.sh
+++ finetune_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 #SBATCH --job-name=finetune
-#SBATCH --output=logs/100kDataExp/out_finetune.txt
-#SBATCH --error=logs/100kDataExp/err_finetune.txt
+#SBATCH --output=logs/100kDataExp/out_without_finetune.txt
+#SBATCH --error=logs/100kDataExp/err_without_finetune.txt
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
@@ -23,6 +23,6 @@ conda activate venv
 echo 'Running experiment...'
 
 echo 'Running Finetuning on Mcrae - fine_tune_mcrae_data_config' 
-python3 fine_tune.py --config_file configs/fine_tune/fine_tune_mcrae_data_config.json
+python3 fine_tune.py --config_file configs/fine_tune/without_finetune_mcrae_data_config copy.json
 
 echo 'finished!'
