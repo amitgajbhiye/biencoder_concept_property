53cb0c137db0dd48df44163e49b4094733511a61
Amit Gajbhiye <amitgajbhiye>
2022-03-07 18:16:32 +0000

    property split exp

--- 3_ft_bert_base_mcrae_fine_tuned_on_prefix_augmented_top_100k_mscg.sh
+++ 3_ft_bert_base_mcrae_fine_tuned_on_prefix_augmented_top_100k_mscg.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ma
 
 #SBATCH --output=logs/100k_property_split_logs/out_ft_bert_base_mcrae_fine_tuned_on_prefix_augmented_top_100k_mscg_model.txt
-#SBATCH --error=logs/100k_property_split_logs/err_cv_bert_base_mcrae_fine_tuned_on_prefix_augmented_top_100k_mscg_model.txt
+#SBATCH --error=logs/100k_property_split_logs/err_ft_bert_base_mcrae_fine_tuned_on_prefix_augmented_top_100k_mscg_model.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
