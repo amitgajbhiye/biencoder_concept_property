3d5712a61ed7ea9ef2bbf95cc002f2b423e773a9
Amit Gajbhiye <amitgajbhiye>
2022-03-14 16:28:10 +0000

    concept_property bert large baselinew/o finetune

--- bert_large_without_ft_run.sh
+++ bert_large_without_ft_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash --login
-#SBATCH --job-name=finetune
+#SBATCH --job-name=bl_w/o_ft
 
-#SBATCH --output=logs/100k_property_split_logs/out_property_split_bert_large_without_ft_baseline.txt
-#SBATCH --error=logs/100k_property_split_logs/err_property_split_bert_large_without_ft_baseline.txt
+#SBATCH --output=logs/100k_concept_property_split_logs/out_cpcv_bert_baseline_without_finetune.txt
+#SBATCH --error=logs/100k_concept_property_split_logs/err_cpcv_bert_baseline_without_finetune.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
@@ -12,7 +12,7 @@
 #SBATCH --mem=15g
 #SBATCH --gres=gpu:1
 
-#SBATCH -t 0-6:00:00
+#SBATCH -t 0-5:00:00
 
 echo 'This script is running on:'
 hostname
@@ -25,7 +25,7 @@ conda activate venv
 
 echo 'Running experiment...'
 
-echo 'Running PCV on BERT LARGE Baseline on Mcrae - configs/fine_tune/pcv_bert_large_without_fine_tune_mcrae_data_config.json' 
-python3 fine_tune.py --config_file configs/fine_tune/pcv_bert_large_without_fine_tune_mcrae_data_config.json
+echo 'Running PCV on BERT LARGE Baseline on Mcrae - configs/fine_tune/cpcv_bert_large_without_fine_tune_mcrae_data_config.json' 
+python3 fine_tune.py --config_file configs/fine_tune/cpcv_bert_large_without_fine_tune_mcrae_data_config.json
 
 echo 'finished!'
