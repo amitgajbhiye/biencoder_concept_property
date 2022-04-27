eea54c3f35f3efa03f6d1bff1050c3df809f7385
Amit Gajbhiye <amitgajbhiye>
2022-04-27 16:28:19 +0100

    cslb con prop config + prop exp run

---
+++ 1_cslb_ft_cpcv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=blmag
+
+#SBATCH --output=logs/cslb_con_prop_split_100k_fine_tuned_logs/out_cslb_ft_bert_large_mscg_plus_prefix_adjective_plus_gkb_100k.txt
+#SBATCH --error=logs/cslb_con_prop_split_100k_fine_tuned_logs/err_cslb_ft_bert_largemscg_plus_prefix_adjective_plus_gkb_100k.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100,gpu
+#SBATCH --mem=16g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 0-20:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+echo 'Running experiment...'
+
+echo 'Bert Large - Training On configs/fine_tune/cslb_cpcv_bert_large_fine_tune_data_config.json' 
+python3 fine_tune.py --config_file configs/fine_tune/cslb_cpcv_bert_large_fine_tune_data_config.json
+
+echo 'finished!'
