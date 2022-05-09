b0c964f6a37d583857c20e29b01c7dbdd6fbf11a
Amit Gajbhiye <amitgajbhiye>
2022-05-09 12:52:24 +0100

    cslb con-prop split bert base no finetune baseline

---
+++ 1_cslb_ft_cpcv_no_fine_tune_bert_base_baseline.sh
@@ -0,0 +1,33 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=cp_bbmag
+
+#SBATCH --output=logs/cslb_fine_tuned_100k_logs/out_cslb_con_prop_split_bert_base_no_finetune_baseline.txt
+#SBATCH --error=logs/cslb_fine_tuned_100k_logs/err_cslb_con_prop_split_bert_base_no_finetune_baseline.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --mem=10g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 2-00:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+echo 'Running experiment...'
+
+echo 'Bert Base Con-Prop Split Bert Base Baseline'
+echo 'Training On configs/fine_tune/cslb_cpcv_bert_base_without_fine_tune_data_config.json'
+python3 fine_tune.py --config_file configs/fine_tune/cslb_cpcv_bert_base_without_fine_tune_data_config.json
+
+echo 'finished!'
