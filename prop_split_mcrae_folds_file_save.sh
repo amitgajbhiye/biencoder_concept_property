15efe11d661de903c2b6f2caf5ab2e57faa25068
Amit Gajbhiye <amitgajbhiye>
2022-05-15 16:54:42 +0100

    prop split fold files save

---
+++ prop_split_mcrae_folds_file_save.sh
@@ -0,0 +1,31 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=PSFS
+
+#SBATCH --output=logs/nn_analysis/out_mcrae_prop_split_roberta_large_baseline.txt
+#SBATCH --error=logs/nn_analysis/err_mcrae_prop_split_roberta_large_baseline.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu,gpu_v100
+#SBATCH --mem=5g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 0-02:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+echo 'Running experiment...'
+
+python3 fine_tune.py --config_file configs/nn_analysis/mcrae_prop_split_file_save_config.json
+
+echo 'finished!'
