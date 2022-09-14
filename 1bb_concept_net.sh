c9a55928fd34c3578907d3bfc38cfa5f0a27e2ac
Amit Gajbhiye <amitgajbhiye>
2022-09-14 16:08:11 +0100

    pretrain 100k wordnet conceptnet

---
+++ 1bb_concept_net.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=100kCN
+
+#SBATCH --output=logs/gkb_source_analysis/out_bb_conceptnet_100k.txt
+#SBATCH --error=logs/gkb_source_analysis/err_bb_conceptnet_100k.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --mem=10g
+#SBATCH --gres=gpu:1
+
+##SBATCH --qos=gpu7d
+#SBATCH -t 2-00:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 run_model.py --config_file configs/gkb_source_analysis/bb_conceptnet_100k_config.json
+
+echo 'finished!'
