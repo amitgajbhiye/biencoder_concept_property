fad3b8981f1fe18bb9274a15b23dfa6062435525
Amit Gajbhiye <amitgajbhiye>
2022-11-22 10:29:33 +0000

    finetuning pcv mcrae - tuplekb & Wordnet

---
+++ 2pcv_bb_mcrae_pcv_tuple_kb_wordnet_all_prop_len.sh
@@ -0,0 +1,28 @@
+#!/bin/bash --login
+#SBATCH --job-name=pcvTkbWn
+
+#SBATCH --output=logs/mcrae_fine_tune_gkb_source_analysis/out_prop_split_bb_mcrae_tuple_kb_wordnet_all_prop_len.txt
+#SBATCH --error=logs/mcrae_fine_tune_gkb_source_analysis/err_prop_split_bb_mcrae_tuple_kb_wordnet_all_prop_len.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --mem=15g
+#SBATCH --gres=gpu:1
+#SBATCH -t 0-12:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 fine_tune.py --config_file configs/fine_tune/gkb_source_analysis/bb_mcrae_pcv_tuple_kb_wordnet_all_prop_len_config.json
+
+echo 'finished!'
