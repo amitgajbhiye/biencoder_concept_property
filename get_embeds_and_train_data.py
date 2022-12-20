27fa8afdd4dd63fbda7d6a5ac1fa693818434243
Amit Gajbhiye <amitgajbhiye>
2022-12-20 18:58:23 +0000

    6_get_predict_property_similiar_props_20neg_70threshold

--- get_embeds_and_train_data.py
+++ get_embeds_and_train_data.py
@@ -501,8 +501,10 @@ def get_predict_prop_similar_properties(
             similar_prop_indices,
         ) = predict_prop_similar_props.kneighbors(zero_embed_predict_prop)
 
+        print(f"similar_prop_indices shape 1: {similar_prop_indices.shape}")
+        print(f"similar_props : {similar_props}")
         similar_prop_indices = np.squeeze(similar_prop_indices)
-        print(f"similar_prop_indices shape : {similar_prop_indices.shape}")
+        print(f"similar_prop_indices shape 2: {similar_prop_indices.shape}")
         # if similar_prop_indices.shape[0] != 1:
         #     similar_prop_indices = np.squeeze(similar_prop_indices)
         # else:
