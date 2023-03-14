adbd4d1aefa67622bd1ee65892ea8736baba930a
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-03-14 11:20:02 +0000

    get embed bert large cnetp trained

--- download_models.sh
+++ download_models.sh
@@ -5,6 +5,9 @@ mkdir -p trained_models/embeddings
 
 cd trained_models
 
+
+echo "Downloading Pretrained Models ..."
+
 # bert-large-uncased trained on conceptnet premium
 wget https://huggingface.co/amitgajbhiye/biencoder_concept_property_pretrained_models/resolve/main/bienc_bert_large_cnetp_pretrain_lr26.pt
 
